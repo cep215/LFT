397305619cbafad704bbdd6c5a6591ea7dacf44a
Theodor Anghel <theo.anghel@gmail.com>
2019-07-02 10:05:47 +0300

    Init Features script in pandas

--- lft/init_features.py
+++ lft/init_features.py
@@ -7,7 +7,7 @@ import pandas as pd
 import numpy as np
 import time
 import math
-from twisted.internet import task, reactor
+# from twisted.internet import task, reactor
 from lft.db_def import Aggregate, Kraken
 
 
@@ -41,7 +41,7 @@ engine = create_engine('sqlite:///' + os.path.join(basedir, 'data.db'), echo=Tru
 Session = sessionmaker(bind=engine)
 session = Session()
 
-period_list = np.array([4320, 1440, 360, 180, 60, 30, 15, 5, 3, 1])
+period_list = np.array([4320, 1440, 360, 180, 60, 30, 15, 5, 3])
 
 
 def get_pandas(db):
@@ -49,77 +49,59 @@ def get_pandas(db):
     df = pd.read_sql(query.statement, session.bind)
     return df
 
-# df_aggregate = get_pandas(Aggregate)
+df = get_pandas(Aggregate)
 # print(df_aggregate.loc[df_aggregate['time'] == '1560857220'])
 
 
-def get_avg(period, start, df):
-    avg = 0
-    for i in range(period) :
-        time = str(start + i*60)
-        # row = session.query(df).filter_by(time=time).first()
-        row = df.loc[df['time'] == time]
-        high = row.high.tolist()[0]
-        low = row.low.tolist()[0]
-        mid = (high + low) / 2
-        avg += mid
-
-    return avg/period
-
-
-
-def get_avg_price(period, timenow, df):
-    p1 = math.ceil(period/24)
-    p2 = math.ceil(2/3 * period/24)
-    avg_1 = get_avg(p1, int(timenow) - period*60, df)
-    avg_2 = get_avg(p2, int(timenow) - p2*60, df)
-    return  avg_1, avg_2
-
-
-def max_price (period, timenow, df):
-    max = 0
-    start = int(timenow) - period * 60
-    for i in range(period) :
-        time = str(start + i*60)
-        # row = session.query(df).filter_by(time=time).first()
-        row = df.loc[df['time'] == time]
-        high = row.high.tolist()[0]
-        if(high > max):
-            max = high
-    return max
-
-def min_price (period, timenow, df):
-    start = int(timenow) - period * 60
-    time = str(start)
-    # row = session.query(df).filter_by(time=time).first()
-    row = df.loc[df['time'] == time]
-    low = row.low.tolist()[0]
-    min = low
-    for i in range(period) :
-        time = str(start + i*60)
-        # row = session.query(df).filter_by(time=time).first()
-        row = df.loc[df['time'] == time]
-        low = row.low.tolist()[0]
-        if(low < min):
-            min = low
-    return min
-
-
-def log_ret (period, timenow, df):
-    avg_1, avg_2 = get_avg_price(period, timenow, df)
-    return math.log(avg_2/avg_1)
-
-
-
-def true_range (period, timenow, df):
-    max = max_price(period, timenow, df)
-    min = min_price(period, timenow, df)
-    return (max - min)/ (max + min)
 
 
 
 
+### Calculate ema for different spans
+df['ema_20'] = df.volumeto.ewm(span = 20, adjust = False).mean()
+
+### Calculate returns on different periods
+for period in period_list:
+    df['returns_'+str(period)] = df['close'].pct_change(periods=period)
+
+### Calculate feature true_range = (max-min)/(max+min) for different periods
+for period in period_list:
+    min = df['low'].rolling(window=period).min()
+    max = df['high'].rolling(window=period).max()
+    df['true_range_'+str(period)] = (max-min)/(max+min)
+
+### Calculate feature log(1 + %returns) ###
+
+df['avg'] = (df['low'] + df['high']) /2
+
+# Getting the average price for interval (start, start+period)
+def get_avg(start, period, df):
+    avg = df['avg'].iloc[start:(start+period)].mean(axis=0)
+    if (period != 0):
+        return avg/period
+    else:
+        return 1
+
+# Calculate log_ret for a period
+def log_ret(period, index, df):
+    p1 = math.ceil(period / 24)
+    p2 = math.ceil(2 / 3 * period / 24)
+    if (index - period > 0):
+        avg_1 = get_avg(index - period, p1, df)
+        avg_2 = get_avg(index - p2, p2, df)
+    else:
+        avg_1 = 1
+        avg_2 = 1
+
+    if (avg_1 != 0):
+        return math.log(avg_2/avg_1)
+
+# Populate df with log_ret_period
+# for period in period_list:
+#     df['log_ret_'+str(period)] = 0
+#     for i in df.index:
+#         df.at[i, 'log_ret_'+str(period)] = log_ret(period, i, df)
 
 
 
-
+print(log_ret(3, 2000, df))
