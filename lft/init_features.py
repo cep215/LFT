668e11aa765c552d568b1e15c29b328939b1d854
cep215 <cep215@ic.ac.uk>
2019-07-03 16:03:29 +0300

    Vectorize for log returns

--- lft/init_features.py
+++ lft/init_features.py
@@ -78,7 +78,7 @@ for period in period_list:
 df['avg'] = (df['low'] + df['high']) /2
 
 # Getting the average price for interval (start, start+period)
-def get_avg(start, period, df):
+def get_avg(start, period):
     avg = df['avg'].iloc[start:(start+period)].mean(axis=0)
     if (period != 0):
         return avg/period
@@ -86,12 +86,13 @@ def get_avg(start, period, df):
         return 1
 
 # Calculate log_ret for a period
-def log_ret(period, index, df):
+def log_ret(period, index):
+
     p1 = math.ceil(period / 24)
     p2 = math.ceil(2 / 3 * period / 24)
     if (index - period > 0):
-        avg_1 = get_avg(index - period, p1, df)
-        avg_2 = get_avg(index - p2, p2, df)
+        avg_1 = get_avg(index - period, p1)
+        avg_2 = get_avg(index - p2, p2)
     else:
         avg_1 = 1
         avg_2 = 1
@@ -99,19 +100,22 @@ def log_ret(period, index, df):
     if (avg_1 != 0):
         return math.log(avg_2/avg_1)
 
+
+
 # Populate df with log_ret_period
-# for period in period_list:
-#     df['log_ret_'+str(period)] = 0
-#     for i in df.index:
-#
-#
-#         df.at[i, 'log_ret_'+str(period)] = log_ret(period, i, df)
+for period in period_list:
+    df['log_ret_'+str(period)] = 0
+    v = np.vectorize(log_ret)
+    df['log_ret_' + str(period)] = v(period, df.index)
 
 
 
 
-#print(log_ret(3, 2000, df))
-print(df[['ema_3','ema_60', 'volumeto']].tail(100))
+
+
+print(log_ret(15, 2003))
+print(df.at[2003, 'log_ret_15'])
+
 
 
 
@@ -143,5 +147,5 @@ for period in period_list:
     df['upper_bb_'+str(period)] = df['ema_close_'+str(period)] + 2*df['std_close_'+str(period)]
 
 
-print(df[['lower_bb_60', 'close','upper_bb_60']].tail(100))
+# print(df[['lower_bb_60', 'close','upper_bb_60']].tail(100))
 
