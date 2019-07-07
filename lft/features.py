cbb5a40dfefc4a8f80ff460db0f390ecadc6ee08
cep215 <cep215@ic.ac.uk>
2019-07-07 17:39:02 +0300

    Update features by minute

--- lft/features.py
+++ lft/features.py
@@ -1,34 +1,41 @@
 import time
 import pandas as pd
 import requests
+import numpy as np
 
-from lft.init_features import *
+
+from lft.init_features import create_past_df, log_ret
+from lft.db_def import Aggregate, Kraken
+
+period_list = np.array([4320, 1440, 360, 180, 60, 30, 15, 5, 3])
+
+alpha = 0.01
+
 
 def closest_time(timenow):
     q = int(timenow / 60)
-
     time1 = 60 * q
-
     if ((timenow * 60) > 0):
         time2 = (60 * (q + 1))
     else:
         time2 = (60 * (q - 1))
-
     if (abs(timenow - time1) < abs(timenow - time2)):
         return time1
-
     return time2
 
-# # Get current time till last minute
-def timestamp_now():
+def get_timestamp_now():
     timenow = time.time()
     return closest_time(timenow)
 
+#########################################################
+df = create_past_df(Aggregate).iloc[-4500:]
+df = df.convert_objects(convert_numeric=True)
+#########################################################
+
+
 # Returns dataframe containing last 10 minutes of data (from cryptocompare api)
 def get_last_record(symbol, comparison_symbol, timestamp, exchange):
-    url = 'https://min-api.cryptocompare.com/data/histominute?fsym={}&tsym={' \
-          '}&limit=10' \
-        .format(symbol.upper(), comparison_symbol.upper())
+    url = 'https://min-api.cryptocompare.com/data/histominute?fsym={}&tsym={}&limit=10'.format(symbol.upper(), comparison_symbol.upper())
     if timestamp:
         url += '&toTs={}'.format(timestamp)
     if exchange:
@@ -42,6 +49,7 @@ def get_last_record(symbol, comparison_symbol, timestamp, exchange):
     else:
         return None
 
+
 # Returns dataframe until given timestamp
 def get_df_until (df, timestamp):
     for index, row in df.iterrows():
@@ -49,25 +57,87 @@ def get_df_until (df, timestamp):
             df.drop(index, inplace=True)
     return df
 
+def update_volume (df_to, df_from, index):
+    last_row = df_to.iloc[index]
+    if (last_row.volumeto == 0 or last_row.volumefrom == 0):
+        row_to_update = df_from.loc[df_from['time'] == last_row.time]
+        df_to.at[df_to.index[index], 'volumeto'] = row_to_update.volumeto
+        df_to.at[df_to.index[index], 'volumefrom'] = row_to_update.volumefrom
+    return df_to
+
 # Returns old dataframe concatanated with new dataframe
-def update_df(df, symbol, comparison_symbol, exchange):
-    df_to_update = get_last_record(symbol, comparison_symbol, timestamp_now(), exchange)
-    df_res = pd.concat([df, df_to_update]).drop_duplicates('time')
+def update_df(df, index, symbol, comparison_symbol, exchange):
+    df_to_update = get_last_record(symbol, comparison_symbol, get_timestamp_now(), exchange)
+
+    df = update_volume(df, df_to_update, index)
+
+    df_res = pd.concat([df, df_to_update]).drop_duplicates(subset='time', keep = 'first').reset_index(drop=True)
+    # print(df_res[['time', 'close', 'volumeto', 'ema_3']])
+
     return df_res
 
 
-# def update():
-#     df_aggregate = get_pandas(Aggregate)
-#     for i in period_list:
-#         df_aggregate['log_ret_' + str(i)] = 0.0
-#         df_aggregate['true_range_' + str(i)] = 0.0
-#
-#     for index, row in df_aggregate.iterrows():
-#         df = get_df_until(df_aggregate, row['time'])
-#         if not df.empty:
-#             for i in period_list:
-#                 df_aggregate[df_aggregate['time'] == row['time']]['log_ret_'
-#                                                                   + str(i)] = \
-#                     log_ret(i, row['time'], df)
-#
-#     print(df_aggregate)
+
+def update_df_features(df, index, symbol, comparison_symbol, exchange):
+    df = update_df(df, index, symbol, comparison_symbol, exchange)
+
+
+    ### Calculate log_ret
+    for period in period_list:
+        df['log_ret_'+str(period)].iloc[index] = 0
+        df['log_ret_' + str(period)].iloc[index] = log_ret(period, df.index[index], df)
+
+
+
+    ### Calculate feature true_range = (max-min)/(max+min) for different periods
+    for period in period_list:
+        # min = df['low'].rolling(window=period).min()
+        min = np.min(df['low'].iloc[(-period + index) : index])
+        # max = df['high'].rolling(window=period).max()
+        max = np.max(df['low'].iloc[(-period + index) : index])
+        df['true_range_' + str(period)].iloc[index] = (max - min) / (max + min)
+
+    ### Calculate feature rel_volume_returns
+    for period in period_list:
+        # Calculate ema for different spans
+        df['ema_volume_' + str(period)].iloc[index] = alpha * df['volumeto'].iloc[index] + (1-alpha) * df['ema_volume_'+ str(period)].iloc[index - 1]
+
+        # Calculate returns on different periods
+        # df['returns_' + str(period)].iloc[index] = df['close'].pct_change(periods=period)
+        df['returns_' + str(period)].iloc[index] = (df['close'].iloc[index] - df['close'].iloc[-period + index]) / df['close'].iloc[-period + index]
+
+        df['rel_volume_returns_' + str(period)].iloc[index] = df['volumeto'].iloc[index] / df['ema_volume_' + str(
+            period)].iloc[index] * df['returns_' + str(period)].iloc[index]
+
+    ### Calculate std_returns
+    for period in period_list:
+        # df['std_returns_' + str(period)].iloc[index] = df['returns_' + str(period)].rolling(window=period).std()
+        df['std_returns_' + str(period)].iloc[index] = np.std(df['returns_' + str(period)].iloc[(-period + index) : index])
+
+    ### Calculate Bollinger Bands
+    for period in period_list:
+        df['ema_close_' + str(period)].iloc[index] = alpha * df['close'].iloc[index] + (1-alpha) * df['ema_close_'+ str(period)].iloc[index - 1]
+        # df['std_close_' + str(period)].iloc[index] = df['close'].rolling(window=period).std()
+        df['std_close_' + str(period)].iloc[index] = np.std(df['close'].iloc[(-period + index) : index])
+
+        df['lower_bb_' + str(period)].iloc[index] = df['ema_close_' + str(period)].iloc[index] - 2 * df['std_close_' + str(period)].iloc[index]
+        df['upper_bb_' + str(period)].iloc[index] = df['ema_close_' + str(period)].iloc[index] + 2 * df['std_close_' + str(period)].iloc[index]
+
+    return df
+
+starttime=time.time()
+
+
+for i in range (-10, 0):
+    df = update_df_features(df, i, 'BTC', 'USD', '')
+
+while True:
+  df = update_df_features(df, -1, 'BTC', 'USD', '')
+  df = df.iloc[1:]
+  print(df[['time', 'close', 'volumeto', 'ema_volume_3']])
+  time.sleep(60.0 - ((time.time() - starttime) % 60.0))
+
+
+
+
+
--- lft/init_features.py
+++ lft/init_features.py
@@ -7,29 +7,11 @@ import pandas as pd
 import numpy as np
 import time
 import math
-# from twisted.internet import task, reactor
 from lft.db_def import Aggregate, Kraken
 
-
-def closest_time(timenow):
-    q = int(timenow / 60)
-
-    time1 = 60 * q
-
-    if ((timenow * 60) > 0):
-        time2 = (60 * (q + 1))
-    else:
-        time2 = (60 * (q - 1))
-
-    if (abs(timenow - time1) < abs(timenow - time2)):
-        return time1
-
-    return time2
-
-# # Get current time till last minute
-def timestamp_now():
-    timenow = time.time()
-    return closest_time(timenow)
+pd.set_option('display.max_rows', 500)
+pd.set_option('display.max_columns', 500)
+pd.set_option('display.width', 1000)
 
 
 
@@ -49,50 +31,24 @@ def get_pandas(db):
     df = pd.read_sql(query.statement, session.bind)
     return df
 
-df = get_pandas(Aggregate)
-# print(df_aggregate.loc[df_aggregate['time'] == '1560857220'])
-
-
-
-### Calculate ema for different spans
-# alpha = 0.01
-for period in period_list:
-    df['ema_'+str(period)] = df.volumeto.ewm(halflife=period, adjust =
-    False).mean()
-
-
-
-
-### Calculate feature true_range = (max-min)/(max+min) for different periods
-for period in period_list:
-    min = df['low'].rolling(window=period).min()
-    max = df['high'].rolling(window=period).max()
-    df['true_range_'+str(period)] = (max-min)/(max+min)
-
-
-
-
-
-### Calculate feature log(1 + %returns) ###
-
-df['avg'] = (df['low'] + df['high']) /2
 
 # Getting the average price for interval (start, start+period)
-def get_avg(start, period):
-    avg = df['avg'].iloc[start:(start+period)].mean(axis=0)
+def get_avg(df, start, period):
+    avg = df['avg'].iloc[start:(start + period)].mean(axis=0)
     if (period != 0):
-        return avg/period
+        return avg / period
     else:
         return 1
 
+
 # Calculate log_ret for a period
-def log_ret(period, index):
+def log_ret(period, index, df):
 
     p1 = math.ceil(period / 24)
     p2 = math.ceil(2 / 3 * period / 24)
     if (index - period > 0):
-        avg_1 = get_avg(index - period, p1)
-        avg_2 = get_avg(index - p2, p2)
+        avg_1 = get_avg(df, index - period, p1)
+        avg_2 = get_avg(df, index - p2, p2)
     else:
         avg_1 = 1
         avg_2 = 1
@@ -100,52 +56,91 @@ def log_ret(period, index):
     if (avg_1 != 0):
         return math.log(avg_2/avg_1)
 
+def create_past_df(db):
 
-
-# Populate df with log_ret_period
-for period in period_list:
-    df['log_ret_'+str(period)] = 0
-    v = np.vectorize(log_ret)
-    df['log_ret_' + str(period)] = v(period, df.index)
+    df = get_pandas(Aggregate)
+    # print(df.loc[df_aggregate['time'] == '1560857220'])
 
 
 
+    ### Calculate ema for different spans
+    # alpha = 0.01
 
-
-
-print(log_ret(15, 2003))
-print(df.at[2003, 'log_ret_15'])
-
-
-
-
-### Calculate feature rel_volume_returns
-for period in period_list:
-    # Calculate ema for different spans
-    df['ema_volume_'+str(period)] = df.volumeto.ewm(span=period, adjust=False).mean()
-
-    # Calculate returns on different periods
-    df['returns_' + str(period)] = df['close'].pct_change(periods=period)
-
-    df['rel_volume_returns_'+str(period)] = df['volumeto']/df['ema_volume_'+str(period)]*df['returns_'+str(period)]
+    for period in period_list:
+        df['ema_'+ str(period)] = df.volumeto.ewm(halflife=period, adjust =
+        False).mean()
 
 
 
 
-### Calculate std_returns
-for period in period_list:
-    df['std_returns_'+str(period)] = df['returns_'+str(period)].rolling(window=period).std()
+    ### Calculate feature true_range = (max-min)/(max+min) for different periods
+    for period in period_list:
+        min = df['low'].rolling(window=period).min()
+        max = df['high'].rolling(window=period).max()
+        df['true_range_'+str(period)] = (max-min)/(max+min)
 
 
 
-### Calculate Bollinger Bands
-for period in period_list:
-    df['ema_close_'+str(period)] = df.close.ewm(span=period, adjust=False).mean()
-    df['std_close_'+str(period)] = df['close'].rolling(window=period).std()
-
-    df['lower_bb_'+str(period)] = df['ema_close_'+str(period)] - 2*df['std_close_'+str(period)]
-    df['upper_bb_'+str(period)] = df['ema_close_'+str(period)] + 2*df['std_close_'+str(period)]
 
 
-# print(df[['lower_bb_60', 'close','upper_bb_60']].tail(100))
+    ### Calculate feature log(1 + %returns) ###
+
+    df['avg'] = (df['low'] + df['high']) /2
+
+
+
+
+
+
+    # Populate df with log_ret_period
+    for period in period_list:
+        df['log_ret_'+str(period)] = 0
+        v = np.vectorize(log_ret)
+        #do not vectorize on df
+        v.excluded.add(2)
+        df['log_ret_' + str(period)] = v(period, df.index, df)
+
+
+
+    # print(log_ret(15, 2003))
+    # print(df.at[2003, 'log_ret_15'])
+
+
+
+
+    ### Calculate feature rel_volume_returns
+    for period in period_list:
+        # Calculate ema for different spans
+        df['ema_volume_'+str(period)] = df.volumeto.ewm(halflife=period, adjust=False).mean()
+
+        # Calculate returns on different periods
+        df['returns_' + str(period)] = df['close'].pct_change(periods=period)
+
+        df['rel_volume_returns_'+str(period)] = df['volumeto']/df['ema_volume_'+str(period)]*df['returns_'+str(period)]
+
+
+
+
+    ### Calculate std_returns
+    for period in period_list:
+        df['std_returns_'+str(period)] = df['returns_'+str(period)].rolling(window=period).std()
+
+
+
+    ### Calculate Bollinger Bands
+    for period in period_list:
+        df['ema_close_'+str(period)] = df.close.ewm(span=period, adjust=False).mean()
+        df['std_close_'+str(period)] = df['close'].rolling(window=period).std()
+
+        df['lower_bb_'+str(period)] = df['ema_close_'+str(period)] - 2*df['std_close_'+str(period)]
+        df['upper_bb_'+str(period)] = df['ema_close_'+str(period)] + 2*df['std_close_'+str(period)]
+
+
+    # print(df[['lower_bb_60', 'close','upper_bb_60']].tail(100))
+
+    return df
+
+
+# print(create_past_df(Aggregate))
+
 
